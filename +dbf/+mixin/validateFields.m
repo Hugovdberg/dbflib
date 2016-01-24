d1ae9dd49b059dc2bf48ee5a857d27fc8c83316e
Hugo van den Berg <hugo@tbdwebdesign.nl>
2016-01-24 23:55:11 +0100

    Rewritten and started in new repository

---
+++ +dbf/+mixin/validateFields.m
@@ -0,0 +1,23 @@
+function fieldsIndex = validateFields(info, fields)
+%VALIDATEFIELDS Determines index of fields to read from DBF.
+
+allFields = {info.FieldInfo.Name};
+if isempty(fields)
+    if ~iscell(fields)
+        % Default case: User omitted the parameter, return all fields.
+        fieldsIndex = 1:info.NumFields;
+    else
+        % User supplied '{}', explicitly skip all fields.
+        fieldsIndex = [];
+    end
+else
+    [reqs, fieldsIndex] = ismember(fields, allFields);
+    if ~all(reqs)
+        warning(...
+            'DBFREAD:InvalidFieldRequest', ...
+            'Requested field ''%s'' does not match DBF field names.', ...
+            fields{~reqs}...
+            )
+        fieldsIndex = fieldsIndex(reqs);
+    end
+end
