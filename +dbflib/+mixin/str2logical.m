3cc9c0ceeed8165dbbecd570d496904e2bac7ddf
Hugo van den Berg <hugo@tbdwebdesign.nl>
2016-03-11 07:25:20 +0100

    fixed wrong definition of a logical field in dbf

--- +dbflib/+mixin/str2logical.m
+++ +dbflib/+mixin/str2logical.m
@@ -1,17 +1,16 @@
 function out = str2logical(in)
 % Convert char array from DBF to cell array of true/false values
+%   
     if isempty(in)
         out = {NaN};
         return
     end
 
-    for k = size(in, 1):-1:1
-        out{k} = empty2nan(logical(sscanf(in(k, :), '%d', 1)));
-    end
-end
-
-function val = empty2nan(val)
-    if isempty(val)
-        val = NaN;
+    truths = {'Y', 'y', 'T', 't'};
+    out = ismember(in, truths);
+    doubtfuls = strcmp(in, '?');
+    if any(doubtfuls)
+        out = double(out);
+        out(doubtfuls) = NaN;
     end
 end
